b9077940eceddf24981c789e1294ed3b5a6fcec0
YuriDuarte <yuri.duarte@totvs.com.br>
2025-06-02 21:49:56 -0300

    feat: update AppComponent to be non-standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: false
 })
 export class AppComponent implements OnInit, OnDestroy {
 
