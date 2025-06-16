fff6fee191f01efe0c25a0bfa2c63e542d284342
YuriDuarte <yuri.duarte@totvs.com.br>
2025-06-16 12:25:59 -0300

    feat: add appConfig with zone change detection and BrowserModule providers

---
+++ src/app/app.config.ts
@@ -0,0 +1,8 @@
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+  importProvidersFrom([BrowserModule])
+  ],
+};
