d1eff3a24c5c1b9896dd1ef6a91ab444bbea0e50
YuriDuarte <yuri.duarte@totvs.com.br>
2025-06-19 16:50:50 -0300

    feat: implement HeaderComponent with template and styles, refactor related components to use it

--- src/app/code-generation-screen/code-generation-screen.component.ts
+++ src/app/code-generation-screen/code-generation-screen.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-code-generation-screen',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './code-generation-screen.component.html',
   styleUrl: './code-generation-screen.component.scss'
 })
--- src/app/code-screen/code-screen.component.ts
+++ src/app/code-screen/code-screen.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-code-screen',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './code-screen.component.html',
   styleUrl: './code-screen.component.scss'
 })
